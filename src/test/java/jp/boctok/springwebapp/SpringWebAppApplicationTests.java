3fcdc6197e303a1754e3fad1c721058249f77762
higashijima mastaka <boctok.ctpoba@gmail.com>
2023-01-29 02:50:03 +0900

    Initial commit

---
+++ src/test/java/jp/boctok/springwebapp/SpringWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.boctok.springwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
